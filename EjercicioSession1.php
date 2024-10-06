1c068c0b5134bc5d178feea1f744fba7bc89fe80
Bryan <bryanjoyarubio@gmail.com>
2024-10-06 15:48:26 +0200

    Update EjercicioSession1.php

--- EjercicioSession1.php
+++ EjercicioSession1.php
@@ -13,21 +13,32 @@
 
         <label for="">
             Nombre del trabajador:
-            <input type="text" name="nombreT" value="pere" class="trabajador">
+            <input type="text" name="nombreT" class="trabajador" required>
         </label>
 
+
+
+
+
         <h1>Choose product</h1>
-        <select name="refresco" id="">
-            <option value="Agua">Agua</option>
-            <option value="Leche">Leche</option>
+
+        <select name="refresco" id="" required>
+            <option value="soft_drink">soft_drink</option>
+            <option value="milk">Leche</option>
         </select>
+
+
+
+
+
 
         <h1>Cantidad Producto</h1>
 
-        <input type="number" name="cantidad" class="cantidad">
+        <input type="number" name="cantidad" class="cantidad" required>
+
         <br><br> <input type="submit" value="add" name="add">
         <input type="submit" value="remove" name="remove">
-        <input type="submit" value="reset" name="reset">
+        <input type="reset" value="reset" name="reset">
 
 
         <h1>Inventario:</h1>
@@ -37,32 +48,56 @@
 </html>
 <?php
 
-if (isset($_POST["nombreT"])) {
+session_start();
 
-    session_start();
+/* echo __LINE__;
+ */
+/* echo "<pre>";
+var_dump($_SESSION);
+echo "</pre>"; */
 
-    $_SESSION["nombreTrabajador"] = $_POST["nombreT"];
 
-    $nombreTrabajador = $_SESSION["nombreTrabajador"];
-
+if (!isset($_SESSION["inventario"])) {
+/*     echo __LINE__;
+ */
+    $_SESSION["inventario"] = [
+        "soft_drink" => 0,
+        "milk" => 0
+    ];
 }
 
-$_SESSION["nombre"] = $nombreTrabajador;
+if (isset($_POST["nombreT"]) && isset($_POST["refresco"]) && isset($_POST["cantidad"])) {
 
-$opcionSeleccionada = $_POST["refresco"];
+    $nombreTrabajador = $_SESSION["nombreTrabajador"] = $_POST["nombreT"];
 
-if (isset($opcionSeleccionada)) {
+    $bebida = $_SESSION["refrescoo"] = $_POST["refresco"];
 
-    
+    $cantidadProducto = $_SESSION["cantidadd"] = $_POST["cantidad"];
+
+    if (isset($_POST["add"])) {
+        $_SESSION["inventario"][$bebida] += $cantidadProducto;
+
+/*         var_dump($_SESSION["inventario"]);
+ *//*         var_dump($_SESSION["inventario"][$bebida]);
+ */    } else if (isset($_POST["remove"])) {
+
+
+        if ($cantidadProducto > $_SESSION["inventario"][$bebida]) {
+
+            $_SESSION["inventario"][$bebida] = 0;
+        } else {
+            $_SESSION["inventario"][$bebida] -= $cantidadProducto;
+        }
+    }
+
+    echo "<br><br>";
+    echo "<p>Producto</p>";
+    echo "Leche: " . $_SESSION["inventario"]["milk"];
+    echo "<br><br>";
+    echo "Agua:" . $_SESSION["inventario"]["soft_drink"];
+} else {
+    echo "Debes de rellenar todos los campos!";
 }
-
-
-
-
-
-
-
-
 
 
 
